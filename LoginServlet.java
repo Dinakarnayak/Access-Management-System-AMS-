e59d2757996a361c7a67f2d2b63a2e485d39449a
Dinakar Nayak. N <122340791+Dinakarnayak@users.noreply.github.com>
2024-10-24 14:28:05 +0530

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,19 @@
+package com.example.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        // Logic to validate user credentials
+        // Redirect based on role
+        response.sendRedirect("requestAccess.jsp"); // Example for employee
+    }
+}
