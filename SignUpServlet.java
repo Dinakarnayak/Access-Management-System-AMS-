08b9083070692b8fab2834d3fde37d30fd1c3e19
Dinakar Nayak. N <122340791+Dinakarnayak@users.noreply.github.com>
2024-10-24 14:26:53 +0530

    Create SignUpServlet.java

---
+++ SignUpServlet.java
@@ -0,0 +1,19 @@
+package com.example.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/signup")
+public class SignUpServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        // Logic to store user in database
+        // Redirect to login page upon success
+        response.sendRedirect("login.jsp");
+    }
+}
